b93221c091fb1bc8a6091dfc716dc849ba34ffb4
Joshua Dela Cruz <delacruz.joshua.dev@gmail.com>
2023-03-15 22:03:58 +0800

    Build color mode toggle switch

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { FiMoon, FiSun } from 'react-icons/fi';
+import { IconButton, useColorMode } from '@chakra-ui/react';
 
 const ColorModeSwitch = () => {
-	return <div>ColorModeSwitch</div>;
+	const { colorMode, toggleColorMode } = useColorMode();
+
+	const icon = colorMode === 'dark' ? <FiSun /> : <FiMoon />;
+
+	return (
+		<IconButton
+			aria-label='Color Mode Toggle'
+			icon={icon}
+			onClick={toggleColorMode}
+		/>
+	);
 };
 
 export default ColorModeSwitch;
