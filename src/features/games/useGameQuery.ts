281266f4879bccae80bdebe796c7dc55db50ecd6
Joshua Dela Cruz <delacruz.joshua.dev@gmail.com>
2023-03-17 19:07:20 +0800

    remove generics for game query store

--- src/features/games/useGameQuery.ts
+++ src/features/games/useGameQuery.ts
@@ -2,12 +2,12 @@ import { create } from 'zustand';
 
 import { GameQuery } from './games';
 
-export interface GameQueryState<T> {
-	gameQuery: T;
-	updateGameQuery: (gameQuery: T) => void;
+export interface GameQueryState {
+	gameQuery: GameQuery;
+	updateGameQuery: (gameQuery: GameQuery) => void;
 }
 
-const useGameQuery = create<GameQueryState<GameQuery>>()((set) => ({
+const useGameQuery = create<GameQueryState>()((set) => ({
 	gameQuery: {} as GameQuery,
 	updateGameQuery: (newGameQuery) =>
 		set((state) => ({ gameQuery: { ...state.gameQuery, ...newGameQuery } })),
