5d4853efade275037127d2453df4c9fd6e036582
Joshua Dela Cruz <delacruz.joshua.dev@gmail.com>
2023-03-21 19:16:02 +0800

    update import order

--- src/features/games/PlatformLinks.tsx
+++ src/features/games/PlatformLinks.tsx
@@ -1,5 +1,5 @@
-import { useRouter } from 'next/router';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 import { Box, Button, Heading, List, ListItem, Stack } from '@chakra-ui/react';
 
 import { Platform } from './games';
