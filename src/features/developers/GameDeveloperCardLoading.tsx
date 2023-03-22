b5eb9f7dca119fe2b73fb77e968123935d3053b6
Joshua Dela Cruz <delacruz.joshua.dev@gmail.com>
2023-03-22 21:20:12 +0800

    update game developer card loading screen

---
+++ src/features/developers/GameDeveloperCardLoading.tsx
@@ -0,0 +1,17 @@
+import { Card, CardBody, HStack, Skeleton } from '@chakra-ui/react';
+
+const GameDeveloperCardLoading = () => {
+	return (
+		<Card rounded='lg' overflow='hidden'>
+			<Skeleton h={200} />
+			<CardBody>
+				<HStack gap={5} justify='space-between'>
+					<Skeleton h={8} w='full' rounded='lg' />
+					<Skeleton boxSize={8} rounded='lg' />
+				</HStack>
+			</CardBody>
+		</Card>
+	);
+};
+
+export default GameDeveloperCardLoading;
--- src/pages/developers/index.tsx
+++ src/pages/developers/index.tsx
@@ -1,9 +1,10 @@
+import { Box, SimpleGrid } from '@chakra-ui/react';
+import { useQuery } from 'react-query';
+
 import client from '@/config/client';
 import { Developer } from '@/features/developers/developer';
-import DeveloperCard from '@/features/developers/DeveloperCard';
+import GameDeveloperCardLoading from '@/features/developers/GameDeveloperCardLoading';
 import GameDevelopersListing from '@/features/developers/GameDevelopersListing';
-import { Box } from '@chakra-ui/react';
-import { useQuery } from 'react-query';
 
 interface GameDevelopersResponse {
 	count: number;
@@ -13,7 +14,7 @@ interface GameDevelopersResponse {
 }
 
 const Developers = () => {
-	const { data: gameDevelopers } = useQuery<
+	const { data: gameDevelopers, isLoading } = useQuery<
 		GameDevelopersResponse,
 		Error,
 		Developer[]
@@ -29,6 +30,14 @@ const Developers = () => {
 
 	return (
 		<Box p={5}>
+			{isLoading && (
+				<SimpleGrid gap={5} columns={{ base: 1, md: 2, lg: 3, xl: 4 }}>
+					{isLoading &&
+						[...Array(10).keys()].map((e) => (
+							<GameDeveloperCardLoading key={e} />
+						))}
+				</SimpleGrid>
+			)}
 			<GameDevelopersListing gameDevelopers={gameDevelopers} />
 		</Box>
 	);
