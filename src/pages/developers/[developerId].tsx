68fb6eee55bbe38d583acfd5337656f9c301c50a
Joshua Dela Cruz <delacruz.joshua.dev@gmail.com>
2023-03-22 23:07:57 +0800

    add padding on developers details skeleton loading

--- src/pages/developers/[developerId].tsx
+++ src/pages/developers/[developerId].tsx
@@ -60,7 +60,7 @@ const GameDeveloperDetails = () => {
 
 	if (isFetchingDeveloperDetails || isFetchingGames)
 		return (
-			<SimpleGrid gap={5} columns={{ base: 1, md: 2, lg: 3, xl: 4 }}>
+			<SimpleGrid p={5} gap={5} columns={{ base: 1, md: 2, lg: 3, xl: 4 }}>
 				{[...Array(10).keys()].map((e) => (
 					<GameCardLoading key={e} />
 				))}
