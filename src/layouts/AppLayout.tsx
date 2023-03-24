2d89ebc168789c4f7b78e7cfe5e2d0e26ae98a0d
Joshua Dela Cruz <delacruz.joshua.dev@gmail.com>
2023-03-24 22:50:29 +0800

    bugfixed page not scrolling to top when navigating to other pages

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-import { Box, Grid, GridItem } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
 
 import Navbar from './Navbar';
 
@@ -9,14 +9,12 @@ interface Props {
 
 const AppLayout = ({ children }: Props) => {
 	return (
-		<Box h='100vh' overflow='auto'>
-			<Grid maxW='7xl' mx='auto' templateRows='auto 1fr'>
-				<GridItem pos='sticky' top={0} zIndex={1000}>
-					<Navbar />
-				</GridItem>
-				<GridItem>{children}</GridItem>
-			</Grid>
-		</Box>
+		<Grid maxW='7xl' mx='auto' templateRows='auto 1fr'>
+			<GridItem pos='sticky' top={0} zIndex={1000}>
+				<Navbar />
+			</GridItem>
+			<GridItem>{children}</GridItem>
+		</Grid>
 	);
 };
 
