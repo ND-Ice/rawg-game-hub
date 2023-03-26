59534bd1e60526764a892c6b8b229f9ff317f840
Joshua Dela Cruz <joshuadelacruz.kabootekph@gmail.com>
2023-03-26 20:12:58 +0800

    update title and website description

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from 'react';
 import { Grid, GridItem } from '@chakra-ui/react';
 
 import Navbar from './Navbar';
+import Head from 'next/head';
 
 interface Props {
 	children: ReactNode;
@@ -9,12 +10,23 @@ interface Props {
 
 const AppLayout = ({ children }: Props) => {
 	return (
-		<Grid maxW='7xl' mx='auto' templateRows='auto 1fr'>
-			<GridItem pos='sticky' top={0} zIndex={1000}>
-				<Navbar />
-			</GridItem>
-			<GridItem>{children}</GridItem>
-		</Grid>
+		<>
+			<Head>
+				<title>RAWG Inspired</title>
+				<meta
+					name='description'
+					content="Welcome to our games website, where you can find a wide range of entertaining and exciting games to play for free! Whether you're looking for action-packed adventure games, brain-teasing puzzle games, or relaxing casual games, we've got you covered."
+				/>
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+			<Grid maxW='7xl' mx='auto' templateRows='auto 1fr'>
+				<GridItem pos='sticky' top={0} zIndex={1000}>
+					<Navbar />
+				</GridItem>
+				<GridItem>{children}</GridItem>
+			</Grid>
+		</>
 	);
 };
 
